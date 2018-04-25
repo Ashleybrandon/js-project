8729bea73bbd390101746ea62d298ea7f2b5e52f
Ashley Brandon <ashleybrandon1@icloud.com>
2018-04-25 17:30:03 +0100

    working tabs
    working tabs

--- script.js
+++ script.js
@@ -16,13 +16,13 @@
     }
     
    menuButton.addEventListener("click", toggleMenu);
-    
-/*    
+       
     
     // logic of tabs
     //tabs - if we click and the click is not on an active tab, we switch tabs
     // we move the 'active' and 'visible' classes to the active tab
     
+    /*
     document.addEventListener('click', tabClick);
     
     function tabClick(event) {
@@ -46,10 +46,67 @@
             }
         }
         
-    }
-    */
-    document.getElementById('tabSection').classList.add('testClass');
+    } */
     
+
+
+// Define a function that filters the unwanted click events on the document
+function tabClick(event) {
+    let elem = event.target,
+        elemHREF = elem.getAttribute('href'),
+        tabs = document.querySelectorAll('.tabs li a'),
+        tabContents = document.querySelectorAll('.tab-contents li');
+
+    // If we click an element whos href contains "tab-", proceed
+    if (elemHREF != null && elemHREF.indexOf('tab-') != -1) {
+        event.preventDefault();
+
+        // If we didn't click an active item, switch tabs
+        if (elem.className.indexOf('active') == -1) {
+            // Remove the active class from the tabs and the visible class from the tab contents
+            for (let i = 0; i < tabs.length; i++) {
+                tabs[i].classList.remove('active');
+                tabContents[i].classList.remove('visible');
+            }
+
+            // Add the active class to the clicked element and the visible class to the corresponding tab
+            elem.classList.add('active');
+            document.getElementById(elemHREF).classList.add('visible');
+        }
+    }
+    
+}
+    
+    
+        // Add an event listener for document click
+document.addEventListener('click', tabClick);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+/*
+    let tabs = document.querySelector('.tabs'),
+     panels = document.querySelector('.panel');
+        tabs.addEventListener('click', function(e){
+            if(e.target.tagName == "LI"){
+                let targetPanel = document.querySelector(e.target.dataset.target);
+                panels.forEach(function(panel){
+                    if (panel == targetPanel){
+                        panel.classList.add('active');
+                    } else {
+                        panel.classList.remove('active');
+                    }
+                })
+            }
+        })
+             */                 
     
     
 })();
