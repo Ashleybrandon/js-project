c0122de79bc032911defbaed088ecb3bd2866f83
Ashley Brandon <ashleybrandon1@icloud.com>
2018-04-30 14:26:39 +0100

    image lightbox

--- script.js
+++ script.js
@@ -16,11 +16,24 @@
     }
     
    menuButton.addEventListener("click", toggleMenu);
+
+   /*
+JQuery menu toggle
+
+$(function(){
+    $('.menu-icon').on('click', function(){
+        $('.mobile-nav').toggleClass('showing')
+    });
+}); 
+
+*/
        
     
 // logic of tabs
 //tabs - if we click and the click is not on an active tab, we switch tabs
 // we move the 'active' and 'visible' classes to the active tab
+// order - function, variables, preventdef, for loop to remove, add classes, 
+// add eventlistener for click and function
   
 
 // a function that defines an event, using event as a paramenter
@@ -54,5 +67,45 @@ function tabClick(event) {
 // Add an event listener for document click
 document.addEventListener('click', tabClick);
 
-    
+
+/*
+ * - Add an event listener for document click
+ * - Define a function that filters the unwanted click events on the document
+ */
+
+// Add an event listener for document click
+document.addEventListener('click', lightboxClick);
+
+// Define a function that filters the unwanted click events on the document
+function lightboxClick(event) {
+    var ele = event.target,
+        eleID = ele.getAttribute('id'),
+        lightboxImg = document.getElementById('lightboxImage'),
+        lightbox = document.getElementById("lightbox-overlay"),
+        newImg = new Image();
+
+    // If we click an element with the attribute "data-lightbox", show the lightbox
+    if (ele.hasAttribute('data-lightbox')) {
+        event.preventDefault();
+
+        newImg.onload = function() {
+            lightboxImg.src = this.src;
+        }
+
+        lightboxImg.src = '';
+        newImg.src = ele.getAttribute('data-lightbox');
+        lightbox.classList.add('visibleb');
+    }
+
+    // If we click any of these 2 elements, close the lightbox
+    if (eleID == 'lightboxImage' || eleID == 'lightbox-overlay') {
+        event.preventDefault();
+
+        lightbox.classList.remove('visibleb');
+    }
+}
+
+
+
 })();
+
