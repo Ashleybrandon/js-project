b07bad75b63d7dee4cc3a8bed2915e687fe8cfc1
Ashley Brandon <ashleybrandon1@icloud.com>
2018-07-31 17:25:05 +0100

    constructor file
    attempting to make a constructor for the toggle function so I can just add new stuff to toggle without writing loads of if else statements

---
+++ constructor.js
@@ -0,0 +1,80 @@
+//create a constructor function for instances of toggling
+//include an event listener etc .. watch video on constructor functions
+/*can i create a var to store the instance, create a function, then fire off any instance i need of the function? sounds like the way forward...*/
+
+/*var sideNav = document.getElementById('sideNav');
+var menuButton = document.getElementById('menuButton');
+
+function toggle() {
+    if(sideNav.style.display) {
+        sideNav.style.display = '';
+    } else {
+        sideNav.style.display = 'block';
+    }
+}
+
+menuButton.addEventListener('click', toggle); 
+
+---------------
+
+var ToggleBlock = function(one, two, three) {
+this.one = one;
+this.two = two;
+this.three = three;
+}
+
+ToggleBlock.prototype.toggle = function() {
+    if(this.style.display) {
+        this.style.display = '';
+    } else {
+        this.style.display = 'block';
+    }
+}
+
+var menuButton = document.getElementById('menuButton');
+var sideNav = new ToggleBlock(document.getElementById('sideNav'));
+
+var sideNav = document.getElementById('sideNav');
+var menuButton = document.getElementById('menuButton');
+
+function toggle(gheh) {
+    this.gheh = gheh;
+    if(gheh.style.display) {
+        gheh.style.display = '';
+    } else {
+        gheh.style.display = 'block';
+    }
+}
+
+var bleh = new toggle(sideNav);
+
+menuButton.addEventListener('click', toggle); 
+
+*/
+
+
+var sideNav = document.getElementById('sideNav');
+var menuButton = document.getElementById('menuButton');
+
+function toggle() {
+    if(sideNav.style.display) {
+        sideNav.style.display = '';
+    } else {
+        sideNav.style.display = 'block';
+    }
+}
+
+menuButton.addEventListener('click', toggle); 
+
+
+
+/*
+JQuery menu toggle
+
+$(function(){
+    $('.menu-icon').on('click', function(){
+        $('.mobile-nav').toggleClass('showing')
+    });
+}); 
+
+*/
