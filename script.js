bd5cd558a536749252e31b82414f6dd879a043f4
Ashley Brandon <ashleybrandon1@icloud.com>
2018-08-08 16:28:17 +0100

    tabs notes

--- script.js
+++ script.js
@@ -5,33 +5,27 @@
        
     
 // logic of tabs
-//tabs - if we click and the click is not on an active tab, we switch tabs
-// we move the 'active' and 'visible' classes to the active tab
-// order - function, variables, preventdef, for loop to remove, add classes, 
-// add eventlistener for click and function
-  
+/* 
 
-// a function that defines an event, using event as a paramenter
-//later passing the function to the click event
-// variables for links, tabs, list items
+next everything in a big function define vars in function prev default if indexOf tab- make a loop that removes classes that show active tab eventTarget.classlist.add(‘active’);
+then outside the function, add event listener w click argument, then name of function
+
+*/
 function tabClick(event) {
     let elem = event.target,
         elemHREF = elem.getAttribute('href'),
         tabs = document.querySelectorAll('.tabs li a'),
         tabContents = document.querySelectorAll('.tab-contents li');
 
-    // If we click an element whos href contains "tab-", cancel the event
-    // that would normally occur as a result of the click
     if (elemHREF.indexOf('tab-') != -1) {
         event.preventDefault();
 
-       //for loop that parses thru the lis of ul.tabs
-       //then removes classes noted
+       
             for (let i = 0; i < tabs.length; i++) {
                 tabs[i].classList.remove('active');
                 tabContents[i].classList.remove('visible');
-            }
-    //add noted classes to the element of the event, and to the link elements
+            };
+
             elem.classList.add('active');
             document.getElementById(elemHREF).classList.add('visible');
     }
